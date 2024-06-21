bd4b7f1ed807d06a60a519d2a2e1e10ab6544110
Seongha <sseongha11@gmail.com>
2024-06-21 18:02:22 +0100

    Adding Spring Data REST Annotation

--- src/main/java/com/recommendation/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/recommendation/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.recommendation.projectboard.repository;
 
 import com.recommendation.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
